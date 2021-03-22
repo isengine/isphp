6affccba9f7e36a7b38d1d1b866a33213f955b3b
fwmakc <fwmakc@mail.ru>
2021-03-22 23:35:45 +0300

    Create template.php

---
+++ php/view/template.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace is\View;
+
+use is\Helpers\System;
+use is\Helpers\Objects;
+
+class Template {
+	
+	public $name;
+	
+	public function __construct() {
+		$this -> name = null;
+	}
+	
+}
+
+?>
