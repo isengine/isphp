c2adbee2acc522c55768849f802132219062cbc8
fwmakc <fwmakc@mail.ru>
2021-11-09 02:27:03 +0300

    Update init.php

--- init.php
+++ init.php
@@ -19,7 +19,7 @@ spl_autoload_register(function($class) {
 	$array = explode('\\', $class);
 	array_shift($array);
 	
-	$path .= __DIR__ . DS . implode(DS, $array) . '.php';
+	$path = __DIR__ . DS . implode(DS, $array) . '.php';
 	if (file_exists($path)) {
 		require $path;
 	}
